709afc5c94e0afcbbff16f55263466ae52c73458
JaseKirby <jasekirby7@gmail.com>
2025-04-15 23:44:47 -0400

    init

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+  kotlin("jvm") version "2.1.10"
+  application
+}
+
+repositories { mavenCentral() }
+
+dependencies {
+  implementation("com.pulumi:pulumi:1.3.0")
+  implementation("com.pulumi:aws:6.77.0")
+
+  testImplementation(kotlin("test"))
+  testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+kotlin { jvmToolchain(23) }
+
+tasks.test { useJUnitPlatform() }
+
+application {
+   mainClass = if (project.hasProperty("mainClass")) project.property("mainClass") as String else "pulkot.AppKt"
+}
---
+++ src/test/kotlin/pulkot/AppTest.kt
@@ -0,0 +1,11 @@
+package pulkot
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class AppTest {
+  @Test
+  fun test() {
+    assertEquals(true, true)
+  }
+}
